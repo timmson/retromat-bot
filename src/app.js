1d9ecb781de11ade4135c7124cd81885afd6a303
timmson <timmson666@mail.ru>
2019-04-21 01:37:07 +0300

    #1

--- src/app.js
+++ src/app.js
@@ -33,8 +33,9 @@ function sendMessage(ctx, activities, i, size) {
             "<b>Описание:</b> " + activity.desc.replace(/<[^>]*>/g, ""),
             "https://retromat.org/ru/?id=" + activity.retromatId
         ].join("\n");
-        log.info("Reply: " + message);
-        if (photos[activity.retromatId] !== undefined && photos[activity.retromatId].length > 0) {
+        log.info("Reply by ID:" + activity.retromatId);
+        log.info(photos[activity.retromatId]);
+        if (photos[activity.retromatId].length > 0) {
             log.info("https://retromat.org/" + photos[activity.retromatId][0].filename);
             ctx.replyWithPhoto({
                 filename: activity.name,
