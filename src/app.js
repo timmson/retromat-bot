23410d71e485b40458f8abc85f51e329ffefb311
timmson <timmson666@mail.ru>
2019-04-21 01:45:25 +0300

    #1

--- src/app.js
+++ src/app.js
@@ -40,7 +40,7 @@ function sendMessage(ctx, i, size) {
             log.info("https://retromat.org/" + photo[0].filename);
             ctx.replyWithPhoto({
                 filename: activity.name,
-                source: "https://retromat.org/" + photo[0].filename
+                url: "https://retromat.org/" + photo[0].filename
             }).then(() =>
                     ctx.replyWithHTML(message).then(() => sendMessage(ctx, ++i, size), (err) => log.error(err)),
                 (err) => {
